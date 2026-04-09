9d8f394169fe10ee7180ada83fec0a9b9b7b524e
KaifKhan3246 <jatukaif0099@gmail.com>
2026-04-09 17:46:26 +0530

    code

---
+++ server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express from 'express'
+import cors from 'cors'
+import userRouter from './routes/userRoute.js';
+import connectDB from './config/mongodb.js';
+import imageRouter from './routes/imageRoute.js';
+
+// App Config
+const PORT = process.env.PORT || 4000
+const app = express();
+await connectDB()
+
+// Intialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API routes
+app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
+
+app.get('/', (req,res) => res.send("API Working"))
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
